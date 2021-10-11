e014bb0bc2f6d06281b4eaec19fb19d98212ed78
ssamba84 <ssamba84@users.noreply.github.com>
2021-10-11 09:47:47 -0700

    Time: 64 ms (24.76%), Space: 16.5 MB (25.69%) - LeetHub

---
+++ diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def helper(root):
+            if root is None:
+                return 0,0
+            lh, ld = helper(root.left)
+            rh, rd = helper(root.right)
+            return 1+max(lh,rh), max(ld, rd, lh+rh+1)
+        return max(helper(root))-1
