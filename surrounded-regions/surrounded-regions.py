fdccd6865bb9c427dfc49c265b5d96bc3f1697d9
ssamba84 <ssamba84@users.noreply.github.com>
2021-11-02 18:41:39 -0700

    Time: 136 ms (79.60%), Space: 16.2 MB (41.46%) - LeetHub

--- surrounded-regions/surrounded-regions.py
+++ surrounded-regions/surrounded-regions.py
@@ -6,26 +6,29 @@ class Solution:
         neighbors = [(1,0),(0,1),(-1,0),(0,-1)]
         R = len(board)
         C = len(board[0])
-        def helper(r,c, ht):
-            
-            if board[r][c] == 'X':
-                return True
-            if (r == 0) or (r == (R-1)) or (c == 0) or (c == (C-1)):
-                return False
-            ret = True
-            ht[(r,c)] = 1
-            for i,j in neighbors:
-                ri = r+i
-                cj = c+j
-                if (ri,cj) not in ht:
-                    ret &= helper(ri,cj, ht)
-            
-            return ret
+        visited = {}
+        def visit(r,c):
+            q = [(r,c)]
+            while len(q) > 0:
+                r,c = q.pop(0)
+                visited[(r,c)] = 1
+                for i,j in neighbors:
+                    ri = r+i
+                    cj = c+j
+                    if 0 <= ri < R and 0 <= cj < C and (ri,cj) not in visited:
+                        if board[ri][cj] == 'O':
+                            visit(ri,cj)
+        for r in range(R):
+            if (r,0) not in visited and board[r][0] == 'O':
+                visit(r,0)
+            if (r,C-1) not in visited and board[r][C-1] == 'O':
+                visit(r,C-1)
+        for c in range(C):
+            if (0,c) not in visited and board[0][c] == 'O':
+                visit(0,c)
+            if (R-1,c) not in visited and board[R-1][c] == 'O':
+                visit(R-1,c)
         for r in range(R):
             for c in range(C):
-                if board[r][c] == 'O':
-                    ht = {}
-                    if helper(r,c,ht):
-                        for i,j in ht.keys():
-                            board[i][j] = 'X'
-            
+                if board[r][c] == 'O' and (r,c) not in visited:
+                    board[r][c] = 'X'
