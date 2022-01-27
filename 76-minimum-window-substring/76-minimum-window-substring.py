e82dfd00d004c4ef938ea4bc3aa36a79dcf67dbf
ssamba84 <ssamba84@users.noreply.github.com>
2022-01-27 07:44:08 -0800

    Time: 133 ms (53.98%), Space: 14.7 MB (89.19%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,33 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        rem = l = r = 0
+        mxlen = math.inf
+        res = ""
         hts = [0]*256
         htt = [0]*256
-        l = r = 0
-        mlen = math.inf
-        ret = ""
-        def check1():
-            for i in range(256):
-                if htt[i] > 0:
-                    if hts[i] < htt[i]:
-                        return True
-            return False
-        def check2():
-            for i in range(256):
-                if htt[i] > 0:
-                    if hts[i] < htt[i]:
-                        return False
-            return True
         for c in t:
-            htt[ord(c)] += 1
-        for r,c in enumerate(s):
             c = ord(c)
+            if htt[c] == 0:
+                rem += 1
+            htt[c] += 1
+        for (r,c) in enumerate(s):
+            c = ord(c)
+            if htt[c] == 0:
+                continue
             hts[c] += 1
-            while check2():
-                #if check2():
-                if mlen > (r-l+1):
-                    ret = s[l:r+1]
-                    mlen = r-l+1
+            if hts[c] == htt[c]:
+                rem -= 1
+            while rem == 0:
                 L = ord(s[l])
-                hts[L] -= 1
+                if mxlen > (r-l+1):
+                    mxlen = (r-l+1)
+                    res = s[l:r+1]
+                if htt[L] != 0:
+                    hts[L] -= 1
+                    if hts[L] < htt[L]:
+                        rem += 1
                 l += 1
-        return ret
+        return res
