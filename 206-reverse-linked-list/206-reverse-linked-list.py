7f5b4ab2d7afaf7a9240142df8ffb8a2899534b9
ssamba84 <ssamba84@users.noreply.github.com>
2022-02-02 12:58:16 -0800

    Time: 50 ms (40.15%), Space: 15.4 MB (90.84%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        revlist = None
+        while head:
+            nxt = head.next
+            head.next = revlist
+            revlist = head
+            head = nxt
+        return revlist
