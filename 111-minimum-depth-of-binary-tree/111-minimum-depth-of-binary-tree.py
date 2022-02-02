890c53f769472d2f0275c7c51b509a9d1e7ddf32
ssamba84 <ssamba84@users.noreply.github.com>
2022-02-02 14:31:43 -0800

    Time: 680 ms (46.62%), Space: 49.5 MB (62.55%) - LeetHub

---
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        q = [(root,1)]
+        res = math.inf
+        while len(q) > 0:
+            node, d = q.pop(0)
+            if node.left is None and node.right is None:
+                res = min(res, d)
+            if node.left:
+                q.append((node.left, d+1))
+            if node.right:
+                q.append((node.right, d+1))
+        return res
