3ebf397b940ccf12795367516c93ce82a09cdad9
ssamba84 <ssamba84@users.noreply.github.com>
2022-02-10 16:56:47 -0800

    Time: 34 ms (76.30%), Space: 13.9 MB (85.07%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.py
@@ -0,0 +1,36 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        ht = {}
+        if len(s) < 2:
+            return s
+        for c in s:
+            ht[c] = ht.get(c,0)+ 1
+        hp = []
+        for k,v in ht.items():
+            heapq.heappush(hp, (-v,k))
+        pf,pc = heapq.heappop(hp)
+        res = ""
+        cc=cf=None
+        '''
+        pf -2
+        pc  a
+        res aba
+        cf -2
+        cc a
+        -2a
+        '''
+        while len(hp) > 0:
+            res += pc
+            pf += 1
+            cf,cc = heapq.heappop(hp)
+            if pc == cc:
+                return ""
+            if pf != 0:
+                heapq.heappush(hp, (pf,pc))
+            pc,pf = cc,cf
+        if pf != -1:
+            return ""
+        return res+pc
+            
+            
+            
