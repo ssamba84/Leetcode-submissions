74e578ca07a58ef952b0b9b72f3faa90583d1e88
ssamba84 <ssamba84@users.noreply.github.com>
2022-02-10 20:51:00 -0800

    Time: 81 ms (74.62%), Space: 13.9 MB (93.32%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -4,7 +4,7 @@ class Solution:
         ht2 = [0]*26
         for c in s1:
             c = ord(c)-ord('a')
-            ht1[c] +=  1
+            ht1[c] += 1
         l = r = 0
         for r,c in enumerate(s2):
             c = ord(c)-ord('a')
@@ -12,8 +12,7 @@ class Solution:
             if (r-l+1) == len(s1):
                 if ht1 == ht2:
                     return True
-                L = ord(s2[l]) - ord('a')
+                L = ord(s2[l])-ord('a')
                 ht2[L] -= 1
                 l += 1
         return False
-        
