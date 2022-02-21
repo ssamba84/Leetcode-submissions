09f778f06bc4c47c50787dc6df1d1a9ac00e8dd2
ssamba84 <ssamba84@users.noreply.github.com>
2022-02-21 10:33:11 -0800

    Time: 210 ms (57.14%), Space: 15.5 MB (43.74%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res = nums[0]
+        count = 1
+        for n in nums[1:]:
+            if res == n:
+                count += 1
+            else:
+                count -= 1
+                if count == 0:
+                    res = n
+                    count = 1
+        return res
