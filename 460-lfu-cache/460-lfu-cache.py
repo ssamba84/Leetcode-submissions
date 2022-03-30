b84b4298f7bab30aec1b5b5c04243190802f4479
ssamba84 <ssamba84@users.noreply.github.com>
2022-03-30 11:21:56 -0700

    Time: 796 ms (98.29%), Space: 78.5 MB (49.25%) - LeetHub

--- 460-lfu-cache/460-lfu-cache.py
+++ 460-lfu-cache/460-lfu-cache.py
@@ -1,101 +1,45 @@
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.freq = 1
-        self.pre = None
-        self.next = None
-
-class DLL:
-    def __init__(self):
-        self.len = 0
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
-        self.head.next = self.tail
-        self.tail.next = self.head
-
-    def addToHead(self, node):
-        temp = self.head.next
-        self.head.next = node
-        node.pre = self.head
-        temp.pre = node
-        node.next = temp
-        self.len +=1
-
-    def removeNode(self, node):
-        tempPre = node.pre
-        tempNext = node.next 
-        tempPre.next = tempNext
-        tempNext.pre = tempPre
-        self.len -=1
-
-    def removeFromTail(self):
-        currNode = self.tail.pre
-        self.removeNode(currNode)
-        return currNode
-        
 class LFUCache:
 
     def __init__(self, capacity: int):
-        self.keyToNode = defaultdict(Node)  
-        self.freqToDLL = defaultdict(DLL)
-        self.capacity = capacity
-        self.minFreq = 0
-
+        self.sz = capacity
+        self.fmap = defaultdict(int)
+        self.minfreq = 0
+        self.ftoOD = defaultdict(OrderedDict)
+    def disp(self):
+        print (self.fmap, self.minfreq, self.ftoOD)
     def get(self, key: int) -> int:
-        if key not in self.keyToNode:
+        
+        if key not in self.fmap:
             return -1
-        # find the current node
-        currNode = self.keyToNode[key]
-        currFreq = currNode.freq
-        currDLL = self.freqToDLL[currFreq]
-        #remove node from currDLL
-        currDLL.removeNode(currNode)
-        
-        # if after remove, currDLL become empty, remove it from map freqToDLL
-        if currDLL.len == 0:
-            del self.freqToDLL[currFreq]
-            
-        #update minFreq if needed
-        if self.minFreq not in self.freqToDLL:
-            self.minFreq +=1
-        
-        #update Node's freq
-        currNode.freq +=1
-        #get newDLL (if newfreq not exist, it will new a DLL)
-        newDLL = self.freqToDLL[currNode.freq] #default value if DLL with len = 1
-        #add node to front of new node
-        newDLL.addToHead(currNode)
-        
-        return currNode.value
-        
+        oldFreq = self.fmap[key]
+        self.fmap[key] += 1
+        od = self.ftoOD[oldFreq]
+        val = od[key]
+        del od[key]
+        if len(od) == 0:
+            del self.ftoOD[oldFreq]
+            if oldFreq == self.minfreq:
+                self.minfreq += 1
+        self.ftoOD[oldFreq+1][key] = val
+        return val
     def put(self, key: int, value: int) -> None:
-        if self.capacity == 0:
-            return
-        
-        
-        # if already exist, update the value
-        if key in self.keyToNode:
-            currNode = self.keyToNode[key]
-            currNode.value = value
-            #don't forget to call get()
-            self.get(key)
+        if self.sz == 0:
+            return 
+        val = self.get(key)
+        if val == -1:
+            if len(self.fmap) == self.sz:
+                k,_ = self.ftoOD[self.minfreq].popitem(0)
+                del self.fmap[k]
+                if len(self.ftoOD[self.minfreq]) == 0:
+                    del self.ftoOD[self.minfreq]
+            self.minfreq = 1
+            self.fmap[key] = 1
+            self.ftoOD[1][key] = value
         else:
-            #if not exit, fist create a new Node
-            currNode = Node(key, value)
-
-            #add node to head of currDLL (which is self.freqToDLL[1])
-            currDLL = self.freqToDLL[1]
-            currDLL.addToHead(currNode)
-
-            #add node to self.keyToNode[key]
-            self.keyToNode[key] = currNode
-
-            #if adding this make over capacity: the size of len(key)== capacity +1, remove from old minFreq's dll's tail
-            if len(self.keyToNode) == self.capacity+1:
-                lfuDLL = self.freqToDLL[self.minFreq]
-                lruNode = lfuDLL.removeFromTail()
-                del self.keyToNode[lruNode.key]
+            self.ftoOD[self.fmap[key]][key] = value
 
 
-            self.minFreq = 1
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
