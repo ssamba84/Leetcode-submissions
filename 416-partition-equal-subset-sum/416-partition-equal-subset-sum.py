5d9c426be54ae29d5624d1e4b92a34320e1201f1
ssamba84 <ssamba84@users.noreply.github.com>
2022-04-01 16:42:33 -0700

    Time: 3406 ms (22.72%), Space: 29.4 MB (43.09%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -3,15 +3,17 @@ class Solution:
         totalsum = sum(nums)
         if totalsum%2 != 0:
             return False
+        
         targetsum = totalsum//2
-        @lru_cache(None)
-        def helper(i, currtotal):
-            if currtotal == 0:
-                return True
-            if i == len(nums):
-                return False
-            ret = False
-            if currtotal >= nums[i]:
-                ret = helper(i+1, currtotal-nums[i])
-            return ret or helper(i+1, currtotal)
-        return helper(0,targetsum)
+        dp = [[False]*(1+targetsum) for _ in range(len(nums))]
+        for r in range(len(nums)):
+            dp[r][0] = True
+        for r in range(len(nums)):
+            for c in range(1,targetsum+1):
+                res = False
+                if r > 0:
+                    res = dp[r-1][c]
+                if c >= nums[r]:
+                    res = res or dp[r-1][c-nums[r]]
+                dp[r][c] = res
+        return dp[-1][-1]
