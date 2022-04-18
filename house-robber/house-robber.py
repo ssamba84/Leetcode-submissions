f5458772f38ff677ebe133af8000734237411ee8
ssamba84 <ssamba84@users.noreply.github.com>
2022-04-18 12:31:51 -0700

    Time: 47 ms, Memory: 14 MB - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        maxret[i] = max(nums[i]+maxret[i-2], maxret[i-1])
-        """
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         if len(nums) == 0:
             return 0
-        if len(nums)< 3:
+        if len(nums) < 3:
             return max(nums)
-        helper = [0]*len(nums)
-        helper[0] = nums[0]
-        helper[1] = max(nums[:2])
-        for i,n in enumerate(nums):
-            if i > 1:
-                helper[i] = max(helper[i-2]+n, helper[i-1])
-        return helper[-1]
+        ppi = nums[0]
+        pi = max(nums[1],nums[0])
+        for i in range(2,len(nums)):
+            ci = max(nums[i]+ppi, pi)
+            pi,ppi = ci,pi
+        return ci
