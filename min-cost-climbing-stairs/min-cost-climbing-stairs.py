eab311ebd140015d01aaafd6ef9612928938cec5
ssamba84 <ssamba84@users.noreply.github.com>
2022-04-18 13:21:59 -0700

    Time: 78 ms, Memory: 13.8 MB - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,13 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        i = 0
-        p0 = cost[0]
-        p1 = cost[1]
-        while i < len(cost):
-            n = cost[i]
-            if i < 2:
-                i += 1
-                continue
-            p1,p0 = min(p1,p0)+n,p1
-            i += 1
-        return min(p1,p0)
+        c0 = cost[0]
+        c1 = cost[1]
+        for i in range(2,len(cost)):
+            cc = min(c0, c1)+cost[i]
+            c0,c1 = c1,cc
+        return min(c0,c1)
