86258584bc4ea6ab1004af8accc828d3868dc61d
ssamba84 <ssamba84@users.noreply.github.com>
2022-04-18 13:24:31 -0700

    Time: 40 ms, Memory: 13.8 MB - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.py
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,9 +1,10 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        t0 = 0
-        t1 = t2 = 1
-        if n < 2:
-            return n
-        for _ in range(n-2):
-            t2,t1,t0 = t2+t1+t0, t2, t1
-        return t2
+        @lru_cache(None)
+        def dp(i):
+            if i <= 0:
+                return 0
+            if 0<i <= 2:
+                return 1
+            return dp(i-1)+dp(i-2)+dp(i-3)
+        return dp(n)
